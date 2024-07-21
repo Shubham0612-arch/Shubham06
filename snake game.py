29b6fa39f7dd136eb3d50aff89040f933996d539
Shubham0612-arch <Shubhamyadav06103@gmail.com>
2024-07-21 23:29:30 +0530

    Snake Game
    using Python

---
+++ snake game.py
@@ -0,0 +1,147 @@
+import turtle
+import time
+import random
+
+DELAY = 0.1
+SCORE = 0
+HIGH_SCORE = 0
+
+# Set up the screen
+win = turtle.Screen()
+win.title("Snake Game")
+win.bgcolor("black")
+win.setup(width=600, height=600)
+win.tracer(0)
+
+# Snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("white")
+head.penup()
+head.goto(0, 0)
+head.direction = "stop"
+
+# Snake food
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0, 100)
+
+segments = []
+
+# Score
+score = turtle.Turtle()
+score.speed(0)
+score.color("white")
+score.penup()
+score.hideturtle()
+score.goto(0, 260)
+score.write("Score: 0  High Score: 0", align="center", font=("Courier", 24, "normal"))
+
+# Functions
+def go_up():
+    if head.direction != "down":
+        head.direction = "up"
+
+def go_down():
+    if head.direction != "up":
+        head.direction = "down"
+
+def go_left():
+    if head.direction != "right":
+        head.direction = "left"
+
+def go_right():
+    if head.direction != "left":
+        head.direction = "right"
+
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+def check_collision():
+    global SCORE, HIGH_SCORE
+    if head.distance(food) < 20:
+        x = random.randint(-290, 290)
+        y = random.randint(-290, 290)
+        food.goto(x, y)
+
+        # Add a new segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("grey")
+        new_segment.penup()
+        segments.append(new_segment)
+
+        SCORE += 10
+
+        if SCORE > HIGH_SCORE:
+            HIGH_SCORE = SCORE
+
+        score.clear()
+        score.write("Score: {}  High Score: {}".format(SCORE, HIGH_SCORE), align="center", font=("Courier", 24, "normal"))
+
+def check_collision_with_body():
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0, 0)
+            head.direction = "stop"
+
+            # Hide the segments
+            for segment in segments:
+                segment.goto(1000, 1000)
+
+            # Clear the segments list
+            segments.clear()
+
+            # Reset the score
+            SCORE = 0
+            score.clear()
+            score.write("Score: {}  High Score: {}".format(SCORE, HIGH_SCORE), align="center", font=("Courier", 24, "normal"))
+
+def move_segments():
+    for index in range(len(segments) - 1, 0, -1):
+        x = segments[index - 1].xcor()
+        y = segments[index - 1].ycor()
+        segments[index].goto(x, y)
+
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x, y)
+
+# Keyboard bindings
+win.listen()
+win.onkeypress(go_up, "w")
+win.onkeypress(go_down, "s")
+win.onkeypress(go_left, "a")
+win.onkeypress(go_right, "d")
+
+# Main game loop
+while True:
+    win.update()
+
+    move()
+    move_segments()
+    check_collision()
+    check_collision_with_body()
+
+    time.sleep(DELAY)
